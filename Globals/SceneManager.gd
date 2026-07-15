a87a29caadd9c993cb5fd5fad9ba03b8907ba333
Sami <physicskabaap2025@gmail.com>
2026-07-15 17:15:43 +0530

    Added The Splash Screen Fonts and Null Scene Handler

--- Globals/SceneManager.gd
+++ Globals/SceneManager.gd
@@ -1,3 +1,26 @@
 # This Scripts Manages the Scenes
-
 extends Node;
+
+# Local Variables
+var scenes: Dictionary;
+
+# Run Once
+func _ready() -> void:
+	# Setup the Scenes
+	scenes = {
+		"main": null
+	};
+
+# Func Which Returns PackedScene
+func init_Scene(scene_name:String) -> PackedScene:
+	var temp_Int: PackedScene;
+	
+	# Check The Scene In The Dic
+	if (scene_name in scenes):
+		if (scenes[scene_name] == null):
+			temp_Int = null
+		else:
+			# TODO Faiq: Init the Scene Here
+			pass;
+	# Return The Packed Scene
+	return temp_Int;
--- Scripts/splash_screen.gd
+++ Scripts/splash_screen.gd
@@ -5,21 +5,34 @@ extends Node
 
 # Exported to Inspector
 @export var Visible_Speed: float = 0.5;
+@export var Exit_Wait_Time: float = 10;
 
 # Constants
+const ERROR: int = 1;
 const MAX_VISIBLE: float = 1.0;
 
 # Local Vars
 var cached_alpha: float;
+var log_t: RichTextLabel;
+var is_Wating: bool;
 
 # Run Once
 func _ready() -> void:
+	# Defaults
+	is_Wating = false;
+	
+	# Cahced The Label
+	log_t = RichTextLabel.new();
+	
 	# Set the Alpha to zero
 	cached_alpha = 0.0;
 	orgName.modulate.a = cached_alpha;
 
 # Run Always
 func _process(delta: float) -> void:
+	# If Timer In Waiting Do Nothing 
+	if (is_Wating):
+		return;
 	
 	# Increase the Alpha Slowely
 	cached_alpha += 0.1 * delta * Visible_Speed; 
@@ -28,4 +41,28 @@ func _process(delta: float) -> void:
 	# If Alpha Max Out Change to Main Menu
 	if (cached_alpha > MAX_VISIBLE):
 		# Change Screen Here
-		pass
+		if (SceneManager.init_Scene("main") != null):
+			# TODO Faiq: Change To The PackedScene Of Main Menu
+			pass;
+		else:
+			# Set The Wating
+			is_Wating = true;
+			
+			# Handle NUll
+			log_t.bbcode_enabled = true;
+			
+			# [Asked Form AI: Question: How to Apply Color in String"
+			log_t.text = "[color=red]Log:[/color] Main Scene NULL";
+			
+			# Pin The Log Lable To The Top Left
+			log_t.anchor_right = 1.0;
+			log_t.anchor_bottom = 1.0;
+			
+			# Add To The Tree
+			add_child(log_t);
+			
+			# Wait for Seconds Before Quit
+			await get_tree().create_timer(Exit_Wait_Time).timeout;
+			
+			# Then Exit With Exit Code
+			get_tree().quit(ERROR);
